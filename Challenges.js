3f8e7c71bf1afb8b813630411b3523b5205b89ae
WinterCenser <165847794+WinterCenser@users.noreply.github.com>
2024-05-01 20:11:58 -0500

    Add files via upload

---
+++ Challenges.js
@@ -0,0 +1,22 @@
+const first = 1
+const second = 2
+
+const third = 6
+const forth = 5
+
+let product = third * forth
+
+console.log( first + second )
+
+console.log ( product / second)
+
+let x = "labia"
+let y = "DrakeBell"
+let temp 
+console.log(x,y)
+
+temp = x
+x = y
+y = temp
+
+console.log(x,y)
